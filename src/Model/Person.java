6bbc1e354a4bfb49dd313744d8abccbec743701f
Newsch00ler <86545418+Newsch00ler@users.noreply.github.com>
2022-05-01 17:19:23 +0800

    Заполнил класс Person

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -1,4 +1,22 @@
 package Model;
 
-public class Person {
+public abstract class Person {
+    protected String surname;
+    protected String name;
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
