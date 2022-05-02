ba686a15ae058131ace20807c16d7e6f5d1f03b3
Newsch00ler <86545418+Newsch00ler@users.noreply.github.com>
2022-05-02 12:31:20 +0800

    Добавил ресурсы и подправил View

--- src/GUI/View.java
+++ src/GUI/View.java
@@ -1,6 +1,8 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
 
 public class View extends JFrame {
     private JTable playersTable;
@@ -10,6 +12,7 @@ public class View extends JFrame {
     private JLabel label2;
     private JLabel label3;
     private JLabel label4;
+    private JLabel background;
 
     private JButton button1;
     private JButton button2;
@@ -18,7 +21,14 @@ public class View extends JFrame {
 
     public View(){
         super("Bookmaker's assistant");
-        setSize(800,400);
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        setIconImage(new ImageIcon(classloader.getResource("Icon.png")).getImage());
+        background = new JLabel(new ImageIcon(classloader.getResource("Background.jpg")));
+        add(background);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setVisible(true);
+        setLocationRelativeTo(null);
+        setResizable(true);
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
     }
 }
